a3abfdee77213f03d1df53a1dccb72fcea290c69
Francisco Gorigoitía <152337783+FranciscoGorigoitia@users.noreply.github.com>
2024-04-08 18:16:56 -0400

    Update main.m

--- main.m
+++ main.m
@@ -1,4 +1,4 @@
-%% I'm calculating the Implied Volatility through Newton Raphson Algorithm. This last one its purpose is to find the implied volatility of option's values.
+%% Calculate the Implied Volatility through Newton Raphson Algorithm. This last one its purpose is to find the implied volatility of option's values.
 clc 
 % Considering an accuracy of 1 bp and different variables for
 % Spot,K,r,q,T,F,Sigma and e. 
@@ -8,13 +8,13 @@ disp(C_P2)
 disp(Vega_P2)
 disp(Sigma_P2)
 
-%replicandolo para la pregunta 3
+% replicandolo para la pregunta 3
 [C_P3,Vega_P3,Sigma_P3]=volBS2(679,700,0.04,0.01,1,98.53,1,1,0.0001);
 disp(C_P3)
 disp(Vega_P3)
 disp(Sigma_P3)
 
-%replicandolo para la pregunta 4
+% replicandolo para la pregunta 4
 [C_P4,Vega_P4,Sigma_P4]=volBS2(679,760,0.04,0.01,0.083,98.53,1,1,0.0001);
 disp(C_P4)
 disp(Vega_P4)
